648a54766ddd21d108dadf2de03b8e495d60d534
tranthudat2k1 <thudatdl123@gmail.com>
2022-11-22 11:32:41 +0700

    split navigator out of App.js

--- App.js
+++ App.js
@@ -1,44 +1,17 @@
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import 'react-native-gesture-handler';
 import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
-import MapScreen from './src/features/restaurants/screens/map.screen'
-import RestaurantScreen from './src/features/restaurants/screens/restaurants.screen'
-import SettingScreen from './src/features/restaurants/screens/setting.screen'
 import { theme } from './src/infrastructure/theme'
 import RestaurantsContextProvider from './src/services/restaurants/restaurants.context'
 import LocationContextProvider from './src/services/location/location.context'
-const Tab = createBottomTabNavigator()
+import AppNavigator from './src/infrastructure/navigation/app.navigator'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <LocationContextProvider>
         <RestaurantsContextProvider>
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-                if (route.name === 'Sichain') {
-                  iconName = focused
-                    ? 'md-restaurant'
-                    : 'md-restaurant-outline';
-                } else if (route.name === 'Setting') {
-                  iconName = focused ? 'md-settings-sharp' : 'md-settings-outline';
-                } else if (route.name === "Map") {
-                  iconName = focused ? 'md-map' : 'md-map-outline';
-                }
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-              tabBarActiveTintColor: 'tomato',
-              tabBarInactiveTintColor: 'gray',
-            })}
-          >
-            <Tab.Screen name="Sichain" component={RestaurantScreen} />
-            <Tab.Screen name="Map" component={MapScreen} />
-            <Tab.Screen name="Setting" component={SettingScreen} />
-          </Tab.Navigator>
+          <AppNavigator />
         </RestaurantsContextProvider>
       </LocationContextProvider>
     </ThemeProvider>
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import MapScreen from '../../features/restaurants/screens/map.screen'
+import RestaurantScreen from '../../features/restaurants/screens/restaurants.screen'
+import SettingScreen from '../../features/restaurants/screens/setting.screen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+const Tab = createBottomTabNavigator()
+
+const AppNavigator = () => {
+  return <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Sichain') {
+          iconName = focused
+            ? 'md-restaurant'
+            : 'md-restaurant-outline';
+        } else if (route.name === 'Setting') {
+          iconName = focused ? 'md-settings-sharp' : 'md-settings-outline';
+        } else if (route.name === "Map") {
+          iconName = focused ? 'md-map' : 'md-map-outline';
+        }
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
+    <Tab.Screen name="Sichain" component={RestaurantScreen} />
+    <Tab.Screen name="Map" component={MapScreen} />
+    <Tab.Screen name="Setting" component={SettingScreen} />
+  </Tab.Navigator>
+}
+
+export default AppNavigator
